84a4080b516efe9c0b01ca764819a8eda019c6b5
juanconizza <juanconizza@gmail.com>
2024-03-08 15:26:12 -0300

    Se ordena codigo

--- juego-ta-te-ti/src/App.jsx
+++ juego-ta-te-ti/src/App.jsx
@@ -1,34 +1,10 @@
 import React, { useState } from "react";
 import { Square } from "./componentes/Square";
+import { calculateWinner } from "./helpers/calculateWinner";
 
 const TURNS = {
   X: "x",
   O: "o"
-};
-
-const calculateWinner = (board) => {
-  const lines = [
-    // Combinaciones de líneas horizontales
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    // Combinaciones de líneas verticales
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    // Combinaciones de líneas diagonales
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
-
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
-      return board[a]; // Retorna el símbolo del ganador (X o O)
-    }
-  }
-
-  return null; // Si no hay un ganador
 };
 
 function App() {
